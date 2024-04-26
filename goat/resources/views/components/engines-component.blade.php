217325dd92c8051d43b0f765b09bfb4d375b3f5a
David Dickinson <rnddave@gmail.com>
2024-04-26 14:57:04 +0100

    basic cards now working (though without data)

---
+++ goat/resources/views/components/engines-component.blade.php
@@ -0,0 +1,11 @@
+<div class="flex size-12 shrink-0 items-center justify-center rounded-full sm:size-16">
+    <img src="./images/f1-engine-3.png" />
+</div>
+
+<div class="pt-3 sm:pt-5 lg:pt-0">
+    <h2 class="text-xl font-semibold text-black dark:text-white">Engines</h2>
+
+    <p class="mt-4 text-sm/relaxed">
+        F1 Engines as a list will go here in order of  most - to least championships.
+    </p>
+</div>
---
+++ goat/resources/views/components/teams-component.blade.php
@@ -0,0 +1,11 @@
+<div class="flex size-12 shrink-0 items-center justify-center rounded-full sm:size-16">
+    <img src="./images/f1-team-3.png" />
+</div>
+
+<div class="pt-3 sm:pt-5 lg:pt-0">
+    <h2 class="text-xl font-semibold text-black dark:text-white">Teams</h2>
+
+    <p class="mt-4 text-sm/relaxed">
+        F1 Teams as a list will go here in order of  most - to least championships.
+    </p>
+</div>
