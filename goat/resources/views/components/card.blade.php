76a5842644c2481bb0b07669a27bafbc37e21965
david <david@discovery.cable.virginm.net>
2024-05-21 16:22:21 +0100

    made a change to the base card component as there was an empty <a> tag which was wrapped around the content of the card itself, the issue this was causing was that when I tried to click on <play the bar chart race> it would try to start the .mp4 file before reloading the page

--- goat/resources/views/components/card.blade.php
+++ goat/resources/views/components/card.blade.php
@@ -1,8 +1,9 @@
-<a
+{{-- <a
     href=""
     id="docs-card"
     class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
->
+> --}}
+<div id="docs-card" class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
     <div class="relative flex items-center gap-6 lg:items-end">
         <div id="docs-card-content" class="flex items-start gap-6 lg:flex-col">
             @if ($cardType == 'drivers')
@@ -14,4 +15,5 @@
             @endif
         </div>
     </div>
-</a>
+</div>
+{{-- </a> --}}
