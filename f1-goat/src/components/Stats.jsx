0c834595589c29a107846241cfd14f93fca0ea39
david <david@discovery.cable.virginm.net>
2024-06-13 14:30:25 +0100

    styling continues, really happy with this now

--- f1-goat/src/components/Stats.jsx
+++ f1-goat/src/components/Stats.jsx
@@ -6,21 +6,34 @@ import ChampionEnginesTable from './ChampionEnginesTable';
 import championDrivers from '../data/championDrivers.json';
 import championConstructors from '../data/championConstructors.json';
 import championEngines from '../data/championEngines.json';
+import driverLogo from '../assets/f1-driver-x.png'; // Import the driver logo
+import teamLogo from '../assets/f1-team-x.png'; // Import the team logo
+import engineLogo from '../assets/f1-engine-x.png'; // Import the engine logo
 
 const Stats = () => {
   return (
     <section className="stats">
       <div className="stats-column">
-        <h2>Top Driver Champions</h2>
-        <ChampionDriversTable data={championDrivers} />
+        <div className="stats-container">
+          <h2>Top Driver Champions</h2>
+          <ChampionDriversTable data={championDrivers} />
+          <img src={driverLogo} alt="F1 Driver Logo" className="floating-logo" />
+        </div>
+      </div>
+
+      <div className="stats-column">
+        <div className="stats-container">
+          <h2>Top Constructor Champions</h2>
+          <ChampionConstructorsTable data={championConstructors} />
+          <img src={teamLogo} alt="F1 Team Logo" className="floating-logo" />
+        </div>
       </div>
       <div className="stats-column">
-        <h2>Top Constructor Champions</h2>
-        <ChampionConstructorsTable data={championConstructors} />
-      </div>
-      <div className="stats-column">
-        <h2>Top Engine Manufacturers</h2>
-        <ChampionEnginesTable data={championEngines} />
+        <div className="stats-container">
+          <h2>Top Engine Manufacturers</h2>
+          <ChampionEnginesTable data={championEngines} />
+          <img src={engineLogo} alt="F1 Engine Logo" className="floating-logo" />
+        </div>
       </div>
     </section>
   );
